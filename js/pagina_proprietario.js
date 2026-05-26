e52c4e89369341b1763526930ddf6e550c065ff0
Geovanna Nunes <geovannanunes349@gmail.com>
2026-05-25 21:10:57 -0300

    repost

--- js/pagina_proprietario.js
+++ js/pagina_proprietario.js
@@ -1,6 +1,6 @@
 // FIREBASE
 import { initializeApp }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+from "";
 
 import {
 
@@ -11,7 +11,7 @@ import {
     onAuthStateChanged
 
 }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+from "";
 
 
 
